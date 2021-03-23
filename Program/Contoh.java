33cc876bc92e82cdf826511c036601644fb23225
bonaventurabs <bonaventurabagas.bb@gmail.com>
2021-03-23 21:21:39 +0700

    revisi Deck Class

--- Program/Contoh.java
+++ Program/Contoh.java
@@ -1,3 +1,21 @@
+/**
+ * Contoh.java
+ * [Kelas Contoh sebagai contoh]
+ */
+
 public class Contoh{
+    /**
+     * Constructor Contoh
+     */
+    public Contoh() {
     
+    }
+
+    /**
+     * Contoh
+     * @param contoh objeck Card
+     */
+    public void moveCardtoDeck(Card contoh) {
+
+    }    
 }
